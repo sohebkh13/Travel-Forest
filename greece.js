20225fbb28bd524179ef0011384e364dfa149e93
amant2020 <37610682+amant2020@users.noreply.github.com>
2021-04-14 20:14:25 +0530

    Add files via upload

---
+++ greece.js
@@ -0,0 +1,93 @@
+
+import React from "react";
+//import "./styles.css"
+import "./Greece.css"
+import Nav from "../Nav";
+//import { Nav, NavItem, NavLink } from 'reactstrap';
+function greece(){
+    return(
+        <div className="greece">
+            <Nav/>
+            <div className="greece-gradient"></div>
+            <div className="greece-image">
+                <div className="greece-description">
+                    <h1><u>Greece</u></h1><br/>
+                    <h4>Greece,a country located in Southeast Europe. Its population is approximately 10.7 million as of 2018; Athens is its largest and capital city, followed by Thessaloniki. Situated on the southern tip of the Balkans, Greece is located at the crossroads of Europe, Asia, and Africa. It shares land borders with Albania to the northwest, North Macedonia and Bulgaria to the north, and Turkey to the northeast. </h4>
+                </div>
+            </div>
+            <div className="greece-image1">
+                <div className="greece-description">
+                    <h1><u>Archaeological Museum, Piraeus</u></h1><br/>
+                    <h4>Just in from the western shore of the Bay of Zea, Piraeus’ Archaeological Museum has artefacts from Mycenaean times to the Roman period, retrieved from across the city and along the coastline of Attica.</h4>
+                </div>
+            </div>
+            <div className="greece-image2">
+                <div className="greece-description">
+                    <h1><u>Bay of Zea (Paşalimanı)</u></h1><br/>
+                    <h4>This bay and natural harbour is the second largest of Piraeus’ three ports. If you have time to spare, you could walk from one side of the harbour entrance to the other, under swaying palm fronds and with lots of interesting little things to look out for.
+</h4>
+                </div>
+            </div>
+            <div className="greece-image3">
+                <div className="greece-description">
+                    <h1><u>Athens</u></h1><br/>
+                    <h4>Although Piraeus is a separate city, Line 1 of the Athens Metro will take you to the heart of the capital in no time at all. For some ancient sightseeing, the Acropolis, Ancient Agora and their catalogue of monuments are spaced fairly tightly together.
+</h4>
+                </div>
+            </div>
+            <div className="greece-image4">
+                <div className="greece-description">
+                <h1><u>Kastella</u></h1><br/>
+                    <h4>Piraeus’ traffic-clogged streets seem a world far away in the Kastella neighbourhood just north and west of Mikrolimano.
+By far the prettiest part of the city, Kastella is a hillside quarter of mansions going back to the 19th century, witnesses to a prosperous past.
+</h4>
+                </div>
+            </div>
+            <div className="greece-image5">
+                <div className="greece-description">
+                    <h1><u>Agia Triada (Church of the Holy Trinity)</u></h1><br/>
+                    <h4>Piraeus’ first Church of the Holy Trinity was founded in 1839 but was completely wiped out in the bombing in 1944. After the war its successor was built on V. Georgios and Ethnikis Antistaseos streets and would eventually be consecrated in the mid-1960s.</h4>
+                </div>
+            </div>
+            <div className="greece-image6">
+                <div className="greece-description">
+                    <h1><u>Kalambaka</u></h1><br/>
+                    <h4>While this small city is primarily visited for its incredible setting beneath the astounding Meteora pinnacles, Kalambaka itself is a pleasant if quiet place to stop by. There is a nice tenth century church to visit as well as some good restaurants and cafes.</h4>
+                </div>
+            </div>
+            <div className="greece-image7">
+                <div className="greece-description">
+                    <h1><u>Great Meteoron Monastery</u></h1><br/>
+                    <h4>Amazing place! It is really incredible how people were building and reaching such places without the help of the modern machinery. Incredible view of the surrounding valley and mountains and other monasteries.</h4>
+                </div>
+            </div>
+			<div className="greece-image8">
+                <div className="greece-description">
+                    <h1><u>Varlaam Monastery</u></h1><br/>
+                    <h4>Founded in 1517, this monastery houses a valuable collection of Byzantine relics.</h4>
+                </div>
+            </div>
+
+            <div className="greece-image9">
+                <div className="greece-description">
+                    <h1><u>Agios Stefanos</u></h1><br/>
+                    <h4>As mentioned, access is far easier than at the other monasteries due to its lower elevation. The art, architecture and views are all magnificent. The nearby monastery at a higher altitude had more expansive views. My qualms over the steps proved unfounded. </h4>
+                </div>
+            </div>
+
+            <div className="greece-image10">
+                <div className="greece-description">
+                    <h1><u>Victoria G. Karelias Collection of Greek Traditional Costumes</u></h1><br/>
+                    <h4>One of the most comprehensive collections
+of Greek traditional costumes
+in Greece, An experiential exhibit,
+a resourceful thematic presentation,
+an advanced media guide tour</h4>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+export default greece
