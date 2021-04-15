edde29b1b6558c55778721e0996bef09aefed01d
amant2020 <37610682+amant2020@users.noreply.github.com>
2021-04-15 10:53:25 +0530

    Add files via upload

---
+++ src/countries/sweden.js
@@ -0,0 +1,85 @@
+
+import React from "react";
+//import "./styles.css"
+import "./Sweden.css"
+import Nav from "../Nav";
+//import { Nav, NavItem, NavLink } from 'reactstrap';
+function Sweden(){
+    return(
+        <div className="sweden">
+            <Nav/>
+            <div className="sweden-gradient"></div>
+            <div className="sweden-image">
+                <div className="sweden-description">
+                    <h1><u>Sweden</u></h1><br/>
+                    <h4>Sweden is a sparsely populated country, characterised by its long coastline, extensive forests and numerous lakes. It is one of the world's northernmost countries. In terms of surface area it is comparable to Spain, Thailand or the American state of California.</h4>
+                </div>
+            </div>
+            <div className="sweden-image1">
+                <div className="sweden-description">
+                    <h1><u>Vasa Museum, Stockholm</u></h1><br/>
+                    <h4>The Vasa Museum (Vasamuseet) in Stockholm is Sweden's most popular museum and now attracts around a million visitors annually. More than 20 million people have visited since the museum opened in 1990, and it's not hard to see why.</h4>
+                </div>
+            </div>
+            <div className="sweden-image2">
+                <div className="sweden-description">
+                    <h1><u>Skansen and Djurgården, Stockholm</u></h1><br/>
+                    <h4>Skansen, the world's oldest open-air museum (and one of the world's largest), is a historic village made up of houses and farmsteads from all over Sweden. Together, these impressively preserved buildings represent both rural and urban culture at various periods from 1720 to the 1960s.</h4>
+                </div>
+            </div>
+            <div className="sweden-image3">
+                <div className="sweden-description">
+                    <h1><u>Gamla Stan, Stockholm</u></h1><br/>
+                    <h4>Stockholm's Old Town, known as Gamla Stan, is a small concentrated area where the city began in the middle of the 13th century. Much of the medieval enclave remains, although in typical Scandinavian style, it is freshly brushed and painted regularly. Its charm is in the architecture along its narrow stone-paved lanes and cobbled streets around its squares, especially the main one, Stortorget, surrounded by old merchants' houses.</h4>
+                </div>
+            </div>
+            <div className="sweden-image4">
+                <div className="sweden-description">
+                <h1><u>Drottningholm Palace, Lovö</u></h1><br/>
+                    <h4>Fairy-tale Drottningholm Palace (Drottningholms slott), on the island of Lovö, is a UNESCO World Heritage Site and lies about 11 kilometers west of Stockholm city center (45 minutes by ferry). Dating from the 17th century, the palace is now the official residence of the Swedish Royal Family.</h4>
+                </div>
+            </div>
+            <div className="sweden-image5">
+                <div className="sweden-description">
+                    <h1><u>Stockholm Archipelago, Stromma Boat Tours</u></h1><br/>
+                    <h4>Stockholm is often referred to as the "Venice of the North." Water is everywhere, and around 30,000 islands lie in Stockholm's wondrous archipelago (skärgården). Distinctive red and yellow timber summerhouses occupy some islands, while others remain totally unspoiled.</h4>
+                </div>
+            </div>
+            <div className="sweden-image6">
+                <div className="sweden-description">
+                    <h1><u> Kiruna and the Ice Hotel, Lapland</u></h1><br/>
+                    <h4>Sharing the same latitude as central Greenland, Kiruna in Lapland is Sweden's northernmost town. It's also the chief town of the largest commune in the country, which borders both Norway and Finland. The midnight sun is visible here from mid-May to mid-July.</h4>
+                </div>
+            </div>
+            <div className="sweden-image7">
+                <div className="sweden-description">
+                    <h1><u>The Göta Canal, Gothenburg to Stockholm</u></h1><br/>
+                    <h4>Often described as Sweden's greatest feat of engineering, the Göta Canal (Göta kanal) dates from the early 19th century and is 190 kilometers in length. It's now one of the country's premier tourist attractions and offers a unique perspective on Sweden's heartland. In addition, by connecting with lakes Vänern and Vättern and the Trollhätte Canal, it forms part of a water link all the way from Stockholm, in the northeast, to Gothenburg, in the southwest.</h4>
+                </div>
+            </div>
+			<div className="sweden-image8">
+                <div className="sweden-description">
+                    <h1><u>Stockholm City Hall</u></h1><br/>
+                    <h4>One of Sweden's most famous buildings, Stockholm City Hall (Stockholms stadshus) was built between 1911 and 1923, using an astonishing eight million bricks. It is considered one of the finest examples of National Romanticism, designed by the architect Ragnar Östberg. The 106-meter-tall tower is topped by three crowns.</h4>
+                </div>
+            </div>
+
+            <div className="sweden-image9">
+                <div className="sweden-description">
+                    <h1><u>Visby, Gotland</u></h1><br/>
+                    <h4>Steeped in medieval history and brimming with ruined churches, the rose-entwined, walled town of Visby, on the island of Gotland, is a huge draw for visitors from around the world. Quaint cobblestone streets snake about the town, and when exploring, it's all too easy to lose your sense of being in the modern world. Adorned with stepped gables, many medieval trading houses remain, as well as some timber buildings from the 17th and 18th centuries.</h4>
+                </div>
+            </div>
+
+            <div className="sweden-image10">
+                <div className="sweden-description">
+                    <h1><u>Liseberg Theme Park, Gothenburg</u></h1><br/>
+                    <h4>Liseberg is one of the most popular destinations in Sweden and each year, the park lures more than three million visitors. It has a huge range of attractions, from children's carousels and a fairy-tale castle to adrenalin-pumping rides for speed demons, bumper cars, and four roller coasters.</h4>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+export default Sweden
---
+++ src/countries/thailand.js
@@ -0,0 +1,87 @@
+
+import React from "react";
+//import "./styles.css"
+import "./Thailand.css"
+import Nav from "../Nav";
+//import { Nav, NavItem, NavLink } from 'reactstrap';
+function Thailand(){
+    return(
+        <div className="thailand">
+            <Nav/>
+            <div className="thailand-gradient"></div>
+            <div className="thailand-image">
+                <div className="thailand-description">
+                    <h1><u>Thailand</u></h1><br/>
+                    <h4>Located in the heart of mainland Southeast Asia, Thailand is a country of mountains, hills, plains and a long coastline along the Gulf of Thailand (1,875 km) and the Andaman Sea (740 km), not including the coastlines of some 400 islands, most of them in the Andaman Sea.</h4>
+                </div>
+            </div>
+            <div className="thailand-image1">
+                <div className="thailand-description">
+                    <h1><u>Railay Beach</u></h1><br/>
+                    <h4>Krabi province is home to some of Thailand's most famous beach destinations—and Railay tops the list as one of the most stunning. Considered by many as one of the best beaches in the country, Railay delivers on promises of white sand, turquoise-blue water, and the feeling that you've found a slice of paradise even before your feet touch the sand.</h4>
+                </div>
+            </div>
+            <div className="thailand-image2">
+                <div className="thailand-description">
+                    <h1><u>Koh Phi Phi</u></h1><br/>
+                    <h4>The Phi Phi Islands are one of Thailand's most popular resort areas for a reason—the clear blue waters, the soft sand, the breathtaking views that go on forever.
+
+You can reach Phi Phi Don—the largest of the islands and the only one permanently inhabited—on a rented kayak or by hiring a small wooden boat to take you here.</h4>
+                </div>
+            </div>
+            <div className="thailand-image3">
+                <div className="thailand-description">
+                    <h1><u>The Grand Palace, Bangkok</u></h1><br/>
+                    <h4>Even if your plans for Thailand mainly involve frolicking on a beach and eating as much Massaman curry and pad Thai as humanly possible, you'll probably spend at least a day or two in Bangkok. There are plenty of things to see and do in the capital, but the Grand Palace should definitely be at the top of your list. This is the number one sightseeing attraction in the city, and it's staggering in both historical significance and craftsmanship.</h4>
+                </div>
+            </div>
+            <div className="thailand-image4">
+                <div className="thailand-description">
+                <h1><u>Sunday Walking Street, Chiang Mai</u></h1><br/>
+                    <h4>Every Thailand visitor looks forward to cheap and delicious food—and that's exactly what they can find in abundance at Chiang Mai's Sunday Night Walking Street. Vendors sell all kinds of treats here: from the popular pad Thai and chicken satay to samosas, fried bananas, sweet roti, and fresh fruit shakes—often for less than $2 a piece.</h4>
+                </div>
+            </div>
+            <div className="thailand-image5">
+                <div className="thailand-description">
+                    <h1><u>Pai</u></h1><br/>
+                    <h4>Thailand's reputation as a country of beautiful landscapes and friendly people is due largely to the world-renowned southern beaches. Because of this, most people don't realize that the vast north is also home to entirely different but equally breathtaking landscapes.</h4>
+                </div>
+            </div>
+            <div className="thailand-image6">
+                <div className="thailand-description">
+                    <h1><u>Wild Elephants at Khao Yai National Park</u></h1><br/>
+                    <h4>Elephants are revered in Thailand, and statues and paintings of them can be seen everywhere you go, including the royal palaces and many temples. For the ultimate experience, however, nothing beats the chance to see elephants in their natural environment—and Khao Yai National Park provides a great opportunity to do just that.</h4>
+                </div>
+            </div>
+            <div className="thailand-image7">
+                <div className="thailand-description">
+                    <h1><u>Sukhothai Old City</u></h1><br/>
+                    <h4>A favorite stop for history buffs and photography enthusiasts, Sukhothai offers many lovely photo ops at a smaller scale than Ayutthaya. Ruins of this old city still stand proud despite enduring centuries of battle and exposure to the elements. Sukhothai's Old City is a UNESCO World Heritage site, and much has been invested to restore and preserve one of Thailand's most significant historical sites.</h4>
+                </div>
+            </div>
+			<div className="thailand-image8">
+                <div className="thailand-description">
+                    <h1><u>Historic City of Ayutthaya</u></h1><br/>
+                    <h4>Ayutthaya offers a magnificent peek into the glory of ancient Thailand, where visitors can wander the haunting but romantic ruins of the former capital.</h4>
+                </div>
+            </div>
+
+            <div className="thailand-image9">
+                <div className="thailand-description">
+                    <h1><u>Doi Suthep</u></h1><br/>
+                    <h4>Perhaps the best-known wat in Chiang Mai sits atop Doi Suthep, a mountain overlooking Thailand's second-largest city. A favorite destination of devout Buddhist followers and travelers from all over the world, Doi Suthep is a marvel of intricate religious carvings—a visit here means seeing monks praying, witnessing worship rituals, and a chance to gaze out over the ever-growing sprawl of Chiang Mai city.</h4>
+                </div>
+            </div>
+
+            <div className="thailand-image10">
+                <div className="thailand-description">
+                    <h1><u>Floating Markets</u></h1><br/>
+                    <h4>Thailand's floating markets offer a unique way to do some shopping and eating while supporting local vendors and getting a closer look into a traditional way of life.</h4>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+export default Thailand
